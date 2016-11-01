7637d4d2abc61b6be431f4432858e9e9f99c1096
854575 <854575@naver.com>
2016-11-01 14:52:55 +0900

    first

---
+++ app/src/main/java/com/example/renya/myapplication_ui/StartActivity.java
@@ -0,0 +1,16 @@
+package com.example.renya.myapplication_ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
+
+/**
+ * Created by Renya on 2016-11-01.
+ */
+public class StartActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ui2);
+    }
+}
