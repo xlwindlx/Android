aa2215a50a4103b7f8f2100cb2f7a41ba5d73006
854575 <854575@naver.com>
2016-11-09 09:35:07 +0900

    first

--- app/src/main/java/com/example/renya/myapplication_ui/StartActivity.java
+++ app/src/main/java/com/example/renya/myapplication_ui/StartActivity.java
@@ -1,9 +1,11 @@
 package com.example.renya.myapplication_ui;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageButton;
+import android.database.sqlite.SQLiteDatabase;
 
 public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
