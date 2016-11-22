06b6ee3f32f9d53f74184459e4012fd8e2a7ede6
korstyx <gallbaron@naver.com>
2016-11-22 14:12:59 +0900

    gallery list
    gallery list

--- app/src/main/java/com/example/renya/myapplication_ui/ImageListActivity.java
+++ app/src/main/java/com/example/renya/myapplication_ui/ImageListActivity.java
@@ -1,14 +1,27 @@
 package com.example.renya.myapplication_ui;
 
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  * Created by Renya on 2016-11-09.
@@ -16,56 +29,145 @@ import android.widget.TextView;
 public class ImageListActivity extends AppCompatActivity {
 
 
-    PagerContainer  mContainer;
+    TextView textTargetUri, textTargetPath1, textTargetPath2;
+    ImageView imageViewfromCam;
+    final static int SELECT_IMAGE =1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menulist1);
 
-        mContainer = (PagerContainer) findViewById(R.id.pager_container);
+        Button buttonLoadImage = (Button)findViewById(R.id.loadimage);
+        textTargetUri = (TextView)findViewById(R.id.targeturi);
+        textTargetPath1 = (TextView)findViewById(R.id.targetpath1);
+        textTargetPath2 = (TextView)findViewById(R.id.targetpath2);
+        imageViewfromCam = (ImageView)findViewById(R.id.imageView);
 
-        ViewPager pager = mContainer.getViewPager();
-        PagerAdapter adapter = new MyPagerAdapter();
-        pager.setAdapter(adapter);
-        //Necessary or the pager will only have one extra page to show
-        // make this at least however many pages you can see
-        pager.setOffscreenPageLimit(adapter.getCount());
-        //A little space between pages
-        pager.setPageMargin(15);
+        buttonLoadImage.setOnClickListener(new View.OnClickListener(){
 
-        //If hardware acceleration is enabled, you should also remove
-        // clipping on the pager for its children.
-        pager.setClipChildren(false);
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, SELECT_IMAGE);
+            }});
     }
 
-    //Nothing special about this adapter, just throwing up colored views for demo
-    private class MyPagerAdapter extends PagerAdapter {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
 
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            TextView view = new TextView(ImageListActivity.this);
-            view.setText("Item "+position);
-            view.setGravity(Gravity.CENTER);
-            view.setBackgroundColor(Color.argb(255, position * 50, position * 10, position * 50));
+        if (resultCode == RESULT_OK){
+            Uri targetUri = data.getData();
+            textTargetUri.setText("Uri: " + targetUri.toString());
+            textTargetPath1.setText("path: " + getPathFromUri_managedQuery(targetUri));
+            textTargetPath2.setText("path: " + getPathFromUri_CursorLoader(targetUri));
 
-            container.addView(view);
-            return view;
+
+            //이미지를 비트맵으로 가져오기
+            Bitmap bitmap=null;
+            if (requestCode == SELECT_IMAGE){
+                Uri image = data.getData();
+                try {
+                    ExifInterface exif = new ExifInterface(getPathFromUri_CursorLoader(targetUri));
+                    int exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                    int exifDegree = exifOrientationToDegrees(exifOrientation);
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),image);
+                    bitmap= rotate(bitmap,exifDegree);
+                }catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+
+
+            imageViewfromCam.setImageBitmap(bitmap);
         }
+    }
 
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView((View)object);
-        }
+    //using deprecated managedQuery() method
+    private String getPathFromUri_managedQuery(Uri uri){
+        String [] projection = {MediaStore.Images.Media.DATA};
 
-        @Override
-        public int getCount() {
-            return 5;
-        }
+        Cursor cursor = managedQuery(
+                uri,
+                projection,
+                null,   //selection
+                null,   //selectionArgs
+                null   //sortOrder
+        );
 
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return (view == object);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+
+        return cursor.getString(column_index);
+    }
+
+    //using CursorLoader() method for API level 11 or higher
+    private String getPathFromUri_CursorLoader(Uri uri){
+
+        String [] projection = {MediaStore.Images.Media.DATA};
+
+        CursorLoader cursorLoader = new CursorLoader(
+                getApplicationContext(),
+                uri,
+                projection,
+                null,   //selection
+                null,   //selectionArgs
+                null   //sortOrder
+        );
+
+        Cursor cursor = cursorLoader.loadInBackground();
+
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+
+        return cursor.getString(column_index);
+    }
+
+    public static Bitmap rotate(Bitmap bitmap, int degrees)
+    {
+        if(degrees != 0 && bitmap != null)
+        {
+            Matrix m = new Matrix();
+            m.setRotate(degrees, (float) bitmap.getWidth() / 2,
+                    (float) bitmap.getHeight() / 2);
+
+            try
+            {
+                Bitmap converted = Bitmap.createBitmap(bitmap, 0, 0,
+                        bitmap.getWidth(), bitmap.getHeight(), m, true);
+                if(bitmap != converted)
+                {
+                    bitmap.recycle();
+                    bitmap = converted;
+                }
+            }
+            catch(OutOfMemoryError ex)
+            {
+                ex.printStackTrace();
+            }
         }
+        return bitmap;
+    }
+
+    public static int exifOrientationToDegrees(int exifOrientation)
+    {
+        if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_90)
+        {
+            return 90;
+        }
+        else if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_180)
+        {
+            return 180;
+        }
+        else if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_270)
+        {
+            return 270;
+        }
+        return 0;
     }
 }
